45d773a68baaf47ffd7e320a06cfc27d75927218
PoisonSyringe <gargarense@hotmail.com>
2024-06-26 22:15:15 -0300

    1.0

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path")
+const app = express();
+
+const port = 80;
+
+const publicPath = path.resolve('./public');
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/home.html"));
+});
+
+app.get("/views/home.html", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/home.html"));
+});
+
+app.get("/views/register.html", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/register.html"));
+});
+
+app.get("/views/login.html", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/login.html"));
+});
+
+app.listen(port, () => {
+    console.log(`online en ${port}`);
+});
